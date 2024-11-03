5fd95a49fbc4c80ef6581d2c2f3e89f15919afea
Tiendat2703 <trantiendatdn78@gmail.com>
2024-11-03 10:34:09 +0700

    Update functions.py

--- model/functions.py
+++ model/functions.py
@@ -18,10 +18,10 @@ def model_LSTM (window_size,num_feature, batch_size, x_train, y_train):
     model.add(Dense(units=1, activation="sigmoid")) # Lớp đầu ra với 1 nơron
     
     # Chọn một ngưỡng để dừng nếu model không cải thiện, và trả về model tốt nhất
-    early_stopping_callback = EarlyStopping(monitor = 'val_loss', # Phụ thuộc vào chỉ số hàm mất mát của validation data
-                                            patience = 5, # Sau 10 epoch mà không cải thiện thì ngừng
-                                            mode = 'min', # min thì là giảm thiểu hàm mất mát, max thì là tối ưu hóa độ chính xác
-                                            min_delta = 0.001, # Ngưỡng tối thiểu để epoch đó được xem là có cải thiện
+    early_stopping_callback = EarlyStopping(monitor = 'val_loss',
+                                            patience = 5, 
+                                            mode = 'min',
+                                            min_delta = 0.001, 
                                             restore_best_weights = True) # Khôi phục lại trọng số tốt nhất của mô hình
     model.compile(optimizer='adam', # Hàm tối ưu hóa là Adam
                   loss='binary_crossentropy', # Nếu dùng nhiều lớp thì dùng sparse_categorical_crossentropy
